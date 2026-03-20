259202cb0944c5fc35617fc31b15edfea4a3dbb8
CJ Winslow <github@cjwinslow.com>
2026-03-20 14:27:40 -0700

    configure vite for tsx and ws proxy

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -1,12 +1,18 @@
 import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
+  plugins: [react()],
   server: {
     port: 7401,
     proxy: {
       "/api": {
         target: "http://localhost:7400",
         changeOrigin: true,
+      },
+      "/ws": {
+        target: "ws://localhost:7400",
+        ws: true,
       },
     },
   },
